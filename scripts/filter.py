fddd75e2ccbd76db3496a683c16b506d545ee3bf
Hassan Umari <oh91@windowslive.com>
2017-03-19 00:39:11 +0400

    update

--- scripts/filter.py
+++ scripts/filter.py
@@ -193,7 +193,7 @@ def node():
 				transformedPoint=tfLisn.transformPoint(globalmaps[i].header.frame_id,temppoint)
 				x=array([transformedPoint.point.x,transformedPoint.point.y])
 				cond=(gridValue(globalmaps[i],x)>threshold) or cond
-			if (cond or (informationGain(mapData,[centroids[z][0],centroids[z][1]],info_radius))<1.0):
+			if (cond or (informationGain(mapData,[centroids[z][0],centroids[z][1]],info_radius*0.5))<0.2):
 				centroids=delete(centroids, (z), axis=0)
 				z=z-1
 			z+=1
